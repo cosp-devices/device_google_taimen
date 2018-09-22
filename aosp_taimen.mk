6c644eee97f82efdc276aa95800ca001a283401d
slothdabski <allthatsleft169@gmail.com>
2018-09-22 12:20:37 +0000

    TARGET_DEVICE := taimen

--- aosp_taimen.mk
+++ aosp_taimen.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+TARGET_DEVICE := taimen
+
 # Include AEX common configuration
 include vendor/aosp/common.mk
 
